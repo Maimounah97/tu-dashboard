91d170273f4836168e4fc730d4ed79b58978b985
Maimounah97 <maealhejaili@gmail.com>
2022-12-21 13:51:44 +0300

    v3

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
+    // $('.selectpicker').selectpicker();
+    // $(".navbar-toggler").click(function () {
+    //     console.log("navbar-toggler clicked")
+    //     $(".sidebar").animate({ width: 'toggle' });
 
-    $(".navbar-toggler").click(function () {
-        console.log("navbar-toggler clicked")
-        $(".sidebar").animate({ width: 'toggle' });
-
-    });
+    // });
     $(".avatar").click(function () {
         console.log("avatar clicked")
         $(".profil-box").children().slideToggle("slow");
@@ -20,10 +20,11 @@ $(document).ready(function () {
         $(".registration-form").hide()
         $(".login-form").animate({ hight: 'toggle' });
     });
-    $(".dropdown-toggle").click(function () {
+    $(".dropdown-toggle").on('click', function () {
         console.log("form clicked")
         $(".menu").animate({ hight: 'toggle' });
-    });
+    })
+
     $(".multible-file-uploade").on('click', '.add-file-upload', function () {
         var file_uploaded = $('#file-uploade-3').html()
         $(".multible-file-uploade").append(file_uploaded)
@@ -43,152 +44,158 @@ $(document).ready(function () {
 
     })
 
-    const barChart = document.getElementById('bar-chart');
-    const pieChart = document.getElementById('pie-chart');
-    const lineChart = document.getElementById('line-chart');
-    const doughnutChart1 = document.getElementById('doughnut-chart-1');
-    const doughnutChart2 = document.getElementById('doughnut-chart-2');
-    const bubbleChart = document.getElementById('bubble-chart');
+    var password = $("password").val();
+    var confirmPassword = $("c-password").val();
+    if (password != confirmPassword) {
+        $(".password-validation").append("<p>test</p>")
+    }
+
+        const barChart = document.getElementById('bar-chart');
+        const pieChart = document.getElementById('pie-chart');
+        const lineChart = document.getElementById('line-chart');
+        const doughnutChart1 = document.getElementById('doughnut-chart-1');
+        const doughnutChart2 = document.getElementById('doughnut-chart-2');
+        const bubbleChart = document.getElementById('bubble-chart');
 
 
-    new Chart(barChart, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+        new Chart(barChart, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
                 }
             }
-        }
-    });
-    new Chart(pieChart, {
-        type: 'pie',
-        data: {
-            labels: [
-                'Red',
-                'Blue',
-                'Yellow'
-            ],
-            datasets: [{
-                label: 'My First Dataset',
-                data: [300, 50, 100],
-                backgroundColor: [
-                    'rgb(3, 30, 69)',
-                    'rgb(54, 162, 235)',
-                    'rgb(229, 235, 247)'
+        });
+        new Chart(pieChart, {
+            type: 'pie',
+            data: {
+                labels: [
+                    'Red',
+                    'Blue',
+                    'Yellow'
                 ],
-                hoverOffset: 4
-            }]
-        }
-    });
-    new Chart(lineChart, {
-        type: 'line',
-        data: {
-            labels: [
-                'January',
-                'February',
-                'March',
-                'April',
-                'May',
-                'June',
-                'July',
-                'August',
-                'September',
-                'October',
-                'November',
-                'December'
-            ],
-            datasets: [{
-                label: 'My First Dataset',
-                data: [65, 59, 80, 81, 56, 55, 10, 80, 90, 85, 70, 30,0,10],
-                fill: false,
-                borderColor: 'rgb(47, 131, 247)',
-                tension: 0.1
-            }]
-        },
+                datasets: [{
+                    label: 'My First Dataset',
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        'rgb(3, 30, 69)',
+                        'rgb(54, 162, 235)',
+                        'rgb(229, 235, 247)'
+                    ],
+                    hoverOffset: 4
+                }]
+            }
+        });
+        new Chart(lineChart, {
+            type: 'line',
+            data: {
+                labels: [
+                    'January',
+                    'February',
+                    'March',
+                    'April',
+                    'May',
+                    'June',
+                    'July',
+                    'August',
+                    'September',
+                    'October',
+                    'November',
+                    'December'
+                ],
+                datasets: [{
+                    label: 'My First Dataset',
+                    data: [65, 59, 80, 81, 56, 55, 10, 80, 90, 85, 70, 30, 0, 10],
+                    fill: false,
+                    borderColor: 'rgb(47, 131, 247)',
+                    tension: 0.1
+                }]
+            },
+
+        });
+        new Chart(doughnutChart1, {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    label: 'My First Dataset',
+                    data: [300, 100],
+                    backgroundColor: [
+                        'rgb(47, 131, 247)',
+                        'rgb(236, 246, 254)',
+                    ],
+                    hoverOffset: 4
+                }]
+            }
+        });
+        new Chart(doughnutChart2, {
+            type: 'doughnut',
+            data: {
+                // labels: [
+                //     'Red',
+                //     'Blue',
+                // ],
+                datasets: [{
+                    label: 'My First Dataset',
+                    data: [300, 50],
+                    backgroundColor: [
+                        'rgb(47, 131, 247)',
+                        'rgb(236, 246, 254)',
+                    ],
+                    hoverOffset: 2
+                }]
+            },
+        });
+        new Chart(bubbleChart, {
+            type: 'bubble',
+            data: {
+                datasets: [{
+                    label: 'First Dataset',
+                    data: [{
+                        x: 20,
+                        y: 30,
+                        r: 7
+                    }, {
+                        x: 40,
+                        y: 10,
+                        r: 7
+                    },
+                    {
+                        x: 30,
+                        y: 17,
+                        r: 7
+                    },
+                    {
+                        x: 50,
+                        y: 20,
+                        r: 7
+                    },
+                    {
+                        x: 60,
+                        y: 1,
+                        r: 7
+                    }
+                    ],
+                    backgroundColor: 'rgb(47, 131, 247)'
+                }],
+            }
+        });
+
+        // doughnutChart2.width = 100;
+        // doughnutChart2.height = 100;
+        // doughnutChart2.resize();
+
 
     });
-    new Chart(doughnutChart1, {
-        type: 'doughnut',
-        data: {
-            datasets: [{
-                label: 'My First Dataset',
-                data: [300, 100],
-                backgroundColor: [
-                    'rgb(47, 131, 247)',
-                    'rgb(236, 246, 254)',
-                ],
-                hoverOffset: 4
-            }]
-        }
-    });
-    new Chart(doughnutChart2, {
-        type: 'doughnut',
-        data: {
-            // labels: [
-            //     'Red',
-            //     'Blue',
-            // ],
-            datasets: [{
-                label: 'My First Dataset',
-                data: [300, 50],
-                backgroundColor: [
-                    'rgb(47, 131, 247)',
-                    'rgb(236, 246, 254)',
-                ],
-                hoverOffset: 2
-            }]
-        },
-    });
-    new Chart(bubbleChart, {
-        type: 'bubble',
-        data: {
-            datasets: [{
-                label: 'First Dataset',
-                data: [{
-                    x: 20,
-                    y: 30,
-                    r: 7
-                }, {
-                    x: 40,
-                    y: 10,
-                    r: 7
-                },
-                {
-                    x: 30,
-                    y: 17,
-                    r: 7
-                },
-                {
-                    x: 50,
-                    y: 20,
-                    r: 7
-                },
-                {
-                    x: 60,
-                    y: 1,
-                    r: 7
-                }
-                ],
-                backgroundColor: 'rgb(47, 131, 247)'
-            }],
-        }
-    });
-
-    // doughnutChart2.width = 100;
-    // doughnutChart2.height = 100;
-    // doughnutChart2.resize();
-
-
-});
 
 
 
